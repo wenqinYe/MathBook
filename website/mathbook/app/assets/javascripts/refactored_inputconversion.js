57c4e0a2e542d998c0c2ab219ef8e24cc4a5ae04
jasonkclee <jasonkjl72@gmail.com>
2017-02-28 08:54:28 -0500

    Added preprocess function and invisible brackets

--- website/mathbook/app/assets/javascripts/refactored_inputconversion.js
+++ website/mathbook/app/assets/javascripts/refactored_inputconversion.js
@@ -1,4 +1,5 @@
-
+const invOpenBracket = "☺"; //character for invisible opening bracket '('
+const invCloseBracket = "☹";//character for invisible closing bracket ')'
 
 function isNumber(str) {
   return !isNaN(str);
@@ -44,6 +45,25 @@ function MathFormatter() {
     this.routine["NONE"] = function(item1, item2){
 }
 
+//preprocess user's input text BEFORE using infixToPostfix on it
+//CURRENTLY: adds invisble opening brackets at ^ or / and inv closing brackets at newline characters
+MathFormatter.prototype.preprocessInput = function(str) {
+  var nBrackets = 0;
+  for(var i = 0; i < str.length; i ++){
+    var char = str[i];
+    if(char == "^" || char == "/"){
+       str = str.substring(0, i+1) + invOpenBracket + str.substring(i+1, str.length);//insert bracket and update index
+      i += 1;
+      nBrackets ++;
+    }
+    else if(char == "\n" && nBrackets > 0){//only close bracket if there is at least 1 opening bracket
+       str = str.substring(0, i) + invCloseBracket + str.substring(i+1, str.length);
+      nBrackets--;//opening bracket is now closed
+      //alert("replaced: " + str);
+       }
+  }
+  return str;
+}
 
   /*
   * Instance methods
@@ -55,6 +75,9 @@ function MathFormatter() {
     var items = [];
     for (var i = 0; i < postfix.length; i++) {
       var char = postfix[i];
+      if(char == ")"){//TEST; wrap brackets last item
+         items[items.length -1] = "(" + items[items.length -1]+")";
+         }
       if (isVariable(char)) {
         items.push(char);
       }
@@ -109,12 +132,14 @@ function MathFormatter() {
           else{
          q.push(char);
           }
-         }else if (char == "(") {
+         }else if (char == "(" || char == invOpenBracket) {
            console.log("opening brace");
            stack.push(char);
-        } else if (char == ")") {
+        } else if (char == ")"|| char == invCloseBracket) {
         var item = stack[stack.length - 1];
-        while (item != "(") {
+        //make sure invCloseBracket matches with InvOpenBracket, NOT JUST normal bracket
+        var openBracket = char == ")"? "(" : invOpenBracket;
+        while (item != openBracket) {
           q.push(item); //push operator into output q
           stack.splice(stack.length - 1, 1); //remove from stack
           item = stack[stack.length - 1];
@@ -123,6 +148,9 @@ function MathFormatter() {
             break;
           }
         }
+        if(char == ")"){  //i.e. if it is NOT the invisible bracket
+            q.push(")");  //SIGNALS that previous operation MUST have brackets around it for outputted text
+          }
         stack.splice(stack.length - 1, 1); //remove left bracket from stack
       } else {
         var p1 = getPrecedence(char);
@@ -157,6 +185,7 @@ $(document).on('input',
     var m = new MathFormatter()
   //alert(isVariable("A"));
     var sIn = String($("#txtIn").val());
+    sIn = m.preprocessInput(sIn);
     katex.render(m.postfixToKatex(m.infixToPostfix(sIn)), $("#divOut").get(0)); //get(0) same as get docelembyid
 
   });
