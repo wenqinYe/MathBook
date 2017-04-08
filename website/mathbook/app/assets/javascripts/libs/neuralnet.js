17546a97e5e55d1890195891ec4e4d6760dd4c17
Your Name <you@example.com>
2017-04-08 13:44:19 -0400

    updated neural net

--- website/mathbook/app/assets/javascripts/libs/neuralnet.js
+++ website/mathbook/app/assets/javascripts/libs/neuralnet.js
@@ -1,14 +1,15 @@
 X = ["log"];
 Y = [""]
 
-l1 = math.random([200, 20], -1, 1)
-l2 = math.random([4, 200], -1, 1)
-layers = [l1, l2]
+w1 = math.random([3, 2], -1, 1)
+w2 = math.random([1, 3], -1, 1)
+weights = [w1, w2]
 
-b1 = math.random([200], -1, 1)
-b2 = math.random([4], -1, 1)
+b1 = math.random([3], -1, 1)
+b2 = math.random([1], -1, 1)
 biases = [b1, b2]
-sigmoids = []
+
+outputs = []
 
 function sigmoid(vector) {
     return vector.map(function(value, index, matrix) {
@@ -21,15 +22,15 @@ var forward = function(input) {
 
     var previous = input
     var output;
-    for (var i = 0; i < layers.length; i++) {
-        var layer = layers[i];
+    for (var i = 0; i < weights.length; i++) {
+        var weight = weights[i];
         var bias = biases[i];
 
-        output = math.multiply(layer, previous);
+        output = math.multiply(weight, previous);
         outout = math.add(output, bias)
         output = sigmoid(output)
 
-        sigmoids.push(output)
+        outputs.push(output)
 
         previous = output
     }
@@ -43,31 +44,31 @@ var backprop = function(output, expected) {
     back = math.multiply(-1, back)
 
     //back prop through hidden layers
-    for (var i = sigmoids.length - 1; i >= 0; i--) {
+    for (var i = outputs.length - 1; i >= 0; i--) {
+        console.log(i)
         //derivative of a sigmoid function
-        sigmoids[i] = sigmoids[i].map(function(value, index, matrix) {
+        outputs[i] = outputs[i].map(function(value, index, matrix) {
             return value * (1 - value)
         });
 
         //dot multiply is elementwise multiplication
-        back = math.dotMultiply(back, sigmoids[i])
+        back = math.dotMultiply(back, outputs[i])
+        console.log("back: ")
+        console.log(back)
 
-
-        var previous = [];
-        for(var j = 0; j < back.length; j++){
-          previous.push(sigmoids[i-1])
-        }
         previous = math.transpose(previous)
+        console.log("previous: " )
+        console.log(previous)
         back = math.multiply(previous, back)
 
-        console.log(back)
-        console.log(layers[i])
-        layers[i] = math.add(math.transpose(back), layers[i])
+        weights[i] = math.add(math.transpose(back), weights[i])
         console.log(layers[i])
     }
 
 }
 
-out = forward(math.random([20]))
+
+
+out = forward(math.random([2]))
 console.log(out)
-backprop(out, math.zeros([4]))
+backprop(out, math.zeros([1]))
